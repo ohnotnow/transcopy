a3d04e3840ae4b4babd5a8221c92a46d87bb0a5c
billy <william.allan@glasgow.ac.uk>
2017-11-20 15:52:40 +0000

    Added pureifycss to strip out unused css classes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,38 @@
-let mix = require('laravel-mix');
-var tailwindcss = require('tailwindcss');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+let cssImport = require('postcss-import')
+let cssNext = require('postcss-cssnext')
+let glob = require('glob-all')
+let mix = require('laravel-mix')
+let purgeCss = require('purgecss-webpack-plugin')
+let tailwind = require('tailwindcss')
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .postCss('resources/assets/css/app.css', 'public/css', [
-       tailwindcss('./tailwind.js'),
-   ]);
+  .postCss('resources/assets/css/app.css', 'public/css/app.css', [
+    cssImport(),
+    tailwind('tailwind.js'),
+    cssNext({ features: { autoprefixer: false }}),
+  ])
+  .version()
+
+if (mix.inProduction()) {
+  mix.webpackConfig({
+    plugins: [
+      new purgeCss({
+        paths: glob.sync([
+          path.join(__dirname, 'resources/views/**/*.blade.php'),
+          path.join(__dirname, 'resources/assets/js/**/*.vue')
+        ]),
+        extractors: [
+          {
+            extractor: class {
+              static extract(content) {
+                return content.match(/[A-z0-9-:\/]+/g)
+              }
+            },
+            extensions: ['html', 'js', 'php', 'vue']
+          }
+        ]
+      })
+    ]
+  })
+}
+
