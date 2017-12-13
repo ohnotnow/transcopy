824f6892d4a27c5e4dcccc1a3b7c87e143d8ecba
billy <william.allan@glasgow.ac.uk>
2017-12-13 10:29:07 +0000

    Formatting

--- resources/assets/js/components/TorrentList.vue
+++ resources/assets/js/components/TorrentList.vue
@@ -1,25 +1,24 @@
-/*
-    - Make torrent list refresh with new torrents every ~10s?
-    - Update incomplete torrents every ~10s?
-    - Auto-copy new complete torrents?
-*/
 <template>
     <div>
         <h3 class="text-xl shadow rounded p-4 bg-grey-lighter">
             <div class="inline-flex items-center">
                 <div class="flex-1 mx-2">
                     <button title="Download" @click="copyTorrents">
-                        <svg class="icon-button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2-8V5h4v5h3l-5 5-5-5h3z"/></svg>
+                        <svg class="icon-button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2-8V5h4v5h3l-5 5-5-5h3z"/>
+                        </svg>
                     </button>
                 </div>
                 <div class="flex-1 mx-2">
                     <a href="#" title="Refresh list" @click="refreshTorrents">
-                        <svg class="icon-button refresh-button" :class="{ spin: refreshing }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.66 15.66A8 8 0 1 1 17 10h-2a6 6 0 1 0-1.76 4.24l1.42 1.42zM12 10h8l-4 4-4-4z"/></svg>
+                        <svg class="icon-button refresh-button" :class="{ spin: refreshing }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path d="M14.66 15.66A8 8 0 1 1 17 10h-2a6 6 0 1 0-1.76 4.24l1.42 1.42zM12 10h8l-4 4-4-4z"/>
+                        </svg>
                     </a>
                 </div>
                 <div class="flex mx-2" v-show="serverError">
                     {{ serverError }}
-                </div>                    
+                </div>
             </div>
         </h3>
 
