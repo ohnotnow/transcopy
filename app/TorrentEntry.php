e27d6ebf891dc5ac52f5d11e8eb5f8cb50051806
billy <william.allan@glasgow.ac.uk>
2017-12-14 15:42:03 +0000

    More tidying

--- app/TorrentEntry.php
+++ app/TorrentEntry.php
@@ -24,38 +24,14 @@ class TorrentEntry extends Model
         CopyFile::dispatch($this);
     }
 
-    public function formattedEta()
+    public function markCopied()
     {
-        if ($this->isComplete()) {
-            return 'Done';
-        }
-
-        if ($this->eta < 0) {
-            return 'Unknown';
-        }
-
-        if ($this->eta < 60) {
-            return "{$this->eta}sec";
-        }
-
-        $mins = intval($this->eta / 60);
-        if ($mins < 60) {
-            return $mins . 'min';
-        }
-
-        $hours = intval($mins / 60);
-        $mins = $mins % 60;
-        return "{$hours}hr {$mins}mins";
+        $this->update(['was_copied' => true, 'is_copying' => false]);
     }
 
-    public function isStillDownloading()
+    public function markCopying()
     {
-        return $this->percent < 100;
-    }
-
-    public function isComplete()
-    {
-        return ! $this->isStillDownloading();
+        $this->update(['is_copying' => true, 'was_copied' => false]);
     }
 
     public function wasAlreadyCopied()
@@ -66,6 +42,16 @@ class TorrentEntry extends Model
     public function isCopying()
     {
         return $this->is_copying;
+    }
+
+    public function isStillDownloading()
+    {
+        return $this->percent < 100;
+    }
+
+    public function isComplete()
+    {
+        return ! $this->isStillDownloading();
     }
 
     public function isFile()
@@ -98,6 +84,12 @@ class TorrentEntry extends Model
         return Storage::disk($this->diskName);
     }
 
+    /**
+     * Return the filename fully prefixed by the storage 'disk' filesystem path
+     *
+     * @param string $filename
+     * @return string
+     */
     public function applyPathPrefix(string $filename) : string
     {
         return $this->source()->getDriver()->getAdapter()->applyPathPrefix($filename);
@@ -108,6 +100,11 @@ class TorrentEntry extends Model
         return $this->source()->exists($this->getBasename());
     }
 
+    /**
+     * Returns an array of files inside 'this' directory.
+     *
+     * @return array
+     */
     public function findFiles()
     {
         if (!$this->source()->exists($this->getPath())) {
@@ -120,16 +117,6 @@ class TorrentEntry extends Model
                 $entry['fullpath'] = $this->applyPathPrefix($entry['path']);
                 return $entry;
             });
-    }
-
-    public function markCopied()
-    {
-        $this->update(['was_copied' => true, 'is_copying' => false]);
-    }
-
-    public function markCopying()
-    {
-        $this->update(['is_copying' => true, 'was_copied' => false]);
     }
 
     public function formattedSize()
@@ -147,6 +134,14 @@ class TorrentEntry extends Model
         return $size . 'bytes';
     }
 
+    /**
+     * Splits filenames with no whitespace so they don't mess with page layout.
+     * Does some extra faff if the filename has an extension at the end so we
+     * don't end up with 'some file name mpg' and return more like
+     * 'some file name.mpg'
+     *
+     * @return string
+     */
     public function webFriendlyName()
     {
         $parts = preg_split('/[\._]+/', $this->getBasename());
@@ -156,5 +151,29 @@ class TorrentEntry extends Model
             $extension = '.' . $extension;
         }
         return implode(' ', $parts) . $extension;
+    }
+
+    public function formattedEta()
+    {
+        if ($this->isComplete()) {
+            return 'Done';
+        }
+
+        if ($this->eta < 0) {
+            return 'Unknown';
+        }
+
+        if ($this->eta < 60) {
+            return "{$this->eta}sec";
+        }
+
+        $mins = intval($this->eta / 60);
+        if ($mins < 60) {
+            return $mins . 'min';
+        }
+
+        $hours = intval($mins / 60);
+        $mins = $mins % 60;
+        return "{$hours}hr {$mins}mins";
     }
 }
