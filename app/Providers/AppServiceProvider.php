17934f0cc14226a5ec8178437c01d5aa3fedf98b
billy <william.allan@glasgow.ac.uk>
2017-12-18 14:09:12 +0000

    Try enabling sqlite's WAL option
    Just to see if it helps with concurrency enough to avoid having
    to use mysql/postgres etc.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->tryEnablingSqliteWal();
+
         $this->app->bind(Transmission::class, function ($app) {
             $client = new Client();
             if (config('transcopy.username')) {
@@ -64,5 +66,24 @@ class AppServiceProvider extends ServiceProvider
         $job = $event->job->payload();
         $jobObj = unserialize($job['data']['command']);
         return $jobObj->file->getBasename();
+    }
+
+    /**
+     * Enable sqlite's WAL journal mode to help concurrency.
+     * Disabled while running phpunit as it breaks in-memory db's.
+     * See :
+     * https://www.sqlite.org/wal.html
+     *
+     * @return void
+     */
+    protected function tryEnablingSqliteWal(Type $var = null)
+    {
+        if (app('env') === 'testing') {
+            return;
+        }
+
+        if (DB::connection() instanceof \Illuminate\Database\SQLiteConnection) {
+            DB::statement(DB::raw('PRAGMA journal_mode = wal;'));
+        }
     }
 }
