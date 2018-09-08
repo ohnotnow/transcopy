9a0faa06d9be6b985b2a77b41a9c3c41f1c5602b
billy <william.allan@glasgow.ac.uk>
2018-09-08 14:52:45 +0100

    Added status for failed copy

--- resources/assets/js/components/Torrent.vue
+++ resources/assets/js/components/Torrent.vue
@@ -28,6 +28,12 @@
       >
         {{ error }}
       </span>
+      <span
+        v-if="copyFailed()"
+        class="text-red pulse"
+      >
+        Copy Failed
+      </span>
     </span>
   </div>
 </template>
@@ -67,10 +73,13 @@ export default {
         return `<svg xmlns="http://www.w3.org/2000/svg" class="icon-small" fill="currentColor" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>`;
       }
       if (this.isCopying()) {
-        return `<svg xmlns="http://www.w3.org/2000/svg" class="icon-small pulse spin" fill="currentColor" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M5.41 16H18a2 2 0 0 0 2-2 1 1 0 0 1 2 0 4 4 0 0 1-4 4H5.41l2.3 2.3a1 1 0 0 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 1 1 1.42 1.4L5.4 16zM6 8a2 2 0 0 0-2 2 1 1 0 0 1-2 0 4 4 0 0 1 4-4h12.59l-2.3-2.3a1 1 0 1 1 1.42-1.4l4 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-1.42-1.4L18.6 8H6z"/></svg>`;
+        return `<svg xmlns="http://www.w3.org/2000/svg" class="icon-small pulse spin" fill="currentColor" viewBox="0 0 20 20"><path class="heroicon-ui" d="M5.41 16H18a2 2 0 0 0 2-2 1 1 0 0 1 2 0 4 4 0 0 1-4 4H5.41l2.3 2.3a1 1 0 0 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 1 1 1.42 1.4L5.4 16zM6 8a2 2 0 0 0-2 2 1 1 0 0 1-2 0 4 4 0 0 1 4-4h12.59l-2.3-2.3a1 1 0 1 1 1.42-1.4l4 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-1.42-1.4L18.6 8H6z"/></svg>`;
       }
       if (this.isQueued()) {
         return `<svg class="icon-small" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M2 2c0-1.1.9-2 2-2h12a2 2 0 0 1 2 2v18l-8-4-8 4V2zm2 0v15l6-3 6 3V2H4z"/></svg>`;
+      }
+      if (this.copyFailed()) {
+        return `<svg class="icon-small text-red pulse" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zM9 5v6h2V5H9zm0 8v2h2v-2H9z"/></svg>`;
       }
       return `<svg class="icon-small" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM6.7 9.29L9 11.6l4.3-4.3 1.4 1.42L9 14.4l-3.7-3.7 1.4-1.42z"/></svg>`;
     }
@@ -119,6 +128,10 @@ export default {
 
     isCopying() {
       return this.theTorrent.copying;
+    },
+
+    copyFailed() {
+      return this.theTorrent.copy_failed;
     },
 
     wasCopied() {
