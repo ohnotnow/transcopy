32d5ad5b3f432da00ddaad8fb7b8373cb237e719
Billy <billybofh@gmail.com>
2019-08-15 17:34:35 +0100

    Minor update to deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,9 @@ NOW=`date +%Y-%m-%d-%H-%M-%S`
 export DOTENV_NAME="transcopy-dotenv-${NOW}"
 export IMAGE_NAME="${DOCKER_ORG}/transcopy:${NOW}"
 export APP_PORT=80
+export PI_IP=192.168.1.86
+export DOCKER_HOST=ssh://${ME}@${PI_IP}
 
 docker buildx build --push --build-arg=PHP_VERSION=${PHP_VERSION} --target=prod --platform linux/amd64,linux/arm/v7 -t ${IMAGE_NAME} .
-cat .env | docker -H ssh://${ME}@192.168.1.86 secret create ${DOTENV_NAME} -
-docker -H ssh://${ME}@192.168.1.86 stack deploy -c docker-stack.yml transcopy
+cat .env | docker secret create ${DOTENV_NAME} -
+docker  stack deploy -c docker-stack.yml transcopy
